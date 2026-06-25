5e800d190d9dec04d18baad600835651cd20d7b4
laraRifaii <rifailara33@gmail.com>
2026-06-25 12:25:12 +0300

    first commit prepared globals.css

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center text-lg bg-zinc-50  dark:bg-black">
+      <div className="font-kyiv text-title">SOLID WOOD PRODUCTS</div>
+      <p className="font-inter text-sm text-body">new paragraph made by me</p>
+    </div>
+  );
+}
