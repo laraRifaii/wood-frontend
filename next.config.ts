c9891d858517da17b91c3d504dde32e70d46f1fa
laraRifaii <rifailara33@gmail.com>
2026-06-28 19:13:24 +0300

    Add cache and dynamic settings to layout; format next.config

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  staticPageGenerationTimeout: 0,
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '3001',
-        pathname: '/uploads/**',
+        protocol: "http",
+        hostname: "localhost",
+        port: "3001",
+        pathname: "/uploads/**",
       },
     ],
   },
